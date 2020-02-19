add80aa25e8033b686a5555b1922dbf5c8c86181
HyugaTsukui <appmail1996@gmail.com>
2020-02-19 21:42:55 +0900

    robcop

--- binary_search.rb
+++ binary_search.rb
@@ -1,29 +1,35 @@
+# frozen_string_literal: true
+
 def binary_search(n:, array:)
-    low = 0
-    high = array.size - 1
-    
-    while low <= high do
-        mid = (low + high) / 2
-        guess = array[mid]
-        
-        if guess.eql? n 
-            return mid
-        elsif n > guess
-            low = mid + 1
-        else
-            high = mid - 1
-        end
+  low = 0
+  high = array.size - 1
+
+  while low <= high
+    mid = (low + high) / 2
+    guess = array[mid]
+
+    if guess.eql? n
+      return mid
+    elsif n > guess
+      low = mid + 1
+    else
+      high = mid - 1
     end
+  end
 end
 
 def binary_search_recursion(n:, array:, low: 0, high: array.size - 1)
-    
-    return nil if array[high] < n or array[low] > n
-    mid = (low + high) / 2
-    guess = array[mid]
-    puts "low = #{low} high = #{high} mid = #{mid} guess = #{guess}"
-    return mid if guess.eql? n
-    return binary_search_recursion(n: n, array: array, high: mid - 1) if guess > n
-    return binary_search_recursion(n: n, array: array, low: mid + 1, high: high) if guess < n
-    
+  return nil if (array[high] < n) || (array[low] > n)
+
+  mid = (low + high) / 2
+  guess = array[mid]
+  puts "low = #{low} high = #{high} mid = #{mid} guess = #{guess}"
+
+  return mid if guess.eql? n
+
+  if guess > n
+    binary_search_recursion(n: n, array: array, high: mid - 1)
+  elsif guess < n
+    binary_search_recursion(n: n, array: array, low: mid + 1, high: high)
+  end
 end
