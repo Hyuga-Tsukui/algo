35e22f5e9e83ff73e6e577e1af69eb46713f11e6
HyugaTsukui <appmail1996@gmail.com>
2020-02-20 22:14:33 +0900

    add test

--- spec/binary_serch_spec.rb
+++ spec/binary_serch_spec.rb
@@ -1,45 +1,47 @@
+# frozen_string_literal: true
+
 require 'spec_helper.rb'
 
 RSpec.describe 'binary_search' do
-    before do
-       @params = { array: (1..65356).to_a } 
-       @min = @params[:array][0]
-       @max = @params[:array][-1]
+  before do
+    @params = { array: (1..65_356).to_a }
+    @min = @params[:array][0]
+    @max = @params[:array][-1]
+  end
+
+  describe '繰り返し処理ver' do
+    it 'choice min is 0' do
+      expect(0).to eq binary_search(@params.merge(n: @min))
     end
-    
-    describe '繰り返し処理ver' do
-       it 'choice min is 0' do
-         expect(0).to eq binary_search(@params.merge(n: @min))
-       end
-       
-       it 'choice max is 65355' do
-         expect(65355).to eq binary_search(@params.merge(n: @max))    
-       end
-       
-       it 'choice min - 1 is nil' do
-         expect(nil).to eq binary_search(@params.merge(n: @min - 1))
-       end
-       
-       it 'choice max + 1 is nil' do
-         expect(nil).to eq binary_search(@params.merge(n: @max + 1))
-       end
+
+    it 'choice max is 65355' do
+      expect(65_355).to eq binary_search(@params.merge(n: @max))
     end
-    
-    describe '再帰処理ver' do
-       it 'choice min is 0' do
-         expect(0).to eq binary_search_recursion(@params.merge(n: @min))
-       end
-       
-       it 'choice max is 65355' do
-         expect(65355).to eq binary_search_recursion(@params.merge(n: @max))    
-       end
-       
-       it 'choice min - 1 is nil' do
-         expect(nil).to eq binary_search_recursion(@params.merge(n: @min - 1))
-       end
-       
-       it 'choice max + 1 is nil' do
-         expect(nil).to eq binary_search_recursion(@params.merge(n: @max + 1))
-       end
+
+    it 'choice min - 1 is nil' do
+      expect(nil).to eq binary_search(@params.merge(n: @min - 1))
     end
+
+    it 'choice max + 1 is nil' do
+      expect(nil).to eq binary_search(@params.merge(n: @max + 1))
+    end
+  end
+
+  describe '再帰処理ver' do
+    it 'choice min is 0' do
+      expect(0).to eq binary_search_recursion(@params.merge(n: @min))
+    end
+
+    it 'choice max is 65355' do
+      expect(65_355).to eq binary_search_recursion(@params.merge(n: @max))
+    end
+
+    it 'choice min - 1 is nil' do
+      expect(nil).to eq binary_search_recursion(@params.merge(n: @min - 1))
+    end
+
+    it 'choice max + 1 is nil' do
+      expect(nil).to eq binary_search_recursion(@params.merge(n: @max + 1))
+    end
+  end
 end
